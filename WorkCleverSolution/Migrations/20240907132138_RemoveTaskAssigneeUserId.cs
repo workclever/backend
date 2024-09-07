f2da93401ef3fe32056ab426bf63a0190e890fc6
ozgur <lugrugzo@gmail.com>
2024-09-07 15:22:21 +0200

    chore: migration `RemoveTaskAssigneeUserId`

---
+++ WorkCleverSolution/Migrations/20240907132138_RemoveTaskAssigneeUserId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkCleverSolution.Migrations
+{
+    public partial class RemoveTaskAssigneeUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssigneeUserId",
+                table: "TaskItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AssigneeUserId",
+                table: "TaskItems",
+                type: "INTEGER",
+                nullable: true);
+        }
+    }
+}
